e0df1643a32c991d4dfe13b873fb1c57676acd58
sfahafi <sfahafidev@gmail.com>
2022-10-22 12:59:49 -0300

    first commit

---
+++ src/main/java/com/sfahafi/asistencia/api/SistemaAsistenciasApiApplication.java
@@ -0,0 +1,13 @@
+package com.sfahafi.asistencia.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAsistenciasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAsistenciasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sfahafi/asistencia/api/SistemaAsistenciasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfahafi.asistencia.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAsistenciasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
